54e8996cfd89d4c1b6dd03c24eb4166573d525aa
Rasel Kibria <rasel5jkibria@gmail.com>
2023-09-21 11:58:32 +0600

    Integer Data type added

---
+++ Basic C# Practice/DataType/Program.cs
@@ -0,0 +1,19 @@
+﻿#region Integer Types
+Console.WriteLine($"sbyte range form {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"byte range form {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"short range form {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"ushort range form {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"int range form {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"uint range form {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"long range form {long.MinValue} to {long.MaxValue}");
+Console.WriteLine($"ulong range form {ulong.MinValue} to {ulong.MaxValue}");
+
+byte centuries = 20;
+ushort years = 2023;
+uint days = 73040;
+ulong hours = 174542321;
+
+Console.WriteLine($"Centuries {centuries}, Years {years}, Days {days}, Hours {hours}");
+
+Console.WriteLine("-------------------------------------------------------");
+#endregion
