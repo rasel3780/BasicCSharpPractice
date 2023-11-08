7b406b1d60d2d06e36843d56d7707f9798f0d427
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-08 23:35:52 +0600

    Method practice added

---
+++ Basic C# Practice/MethodPractice/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+
+static void ShowName(string name)
+{
+    Console.WriteLine(name);
+}
+
+ShowName("Rasel");
+
+static void AddTwoNumber(double num1, double num2)
+{
+    Console.WriteLine(num1+num2);
+}
+
+AddTwoNumber(2, 3);
+AddTwoNumber(5, 5);
