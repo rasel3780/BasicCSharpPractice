396f2aa2505439ef764b7c2496efaf7ade83f665
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-08 21:33:49 +0600

    Simple Arithmetic Calculator added

---
+++ Basic C# Practice/SimpleArithmetic/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace SimpleArithmetic
+{
+    partial class Calulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.number1TxtBox = new System.Windows.Forms.TextBox();
+            this.number2TxtBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.subtractBtn = new System.Windows.Forms.Button();
+            this.multiplyBtn = new System.Windows.Forms.Button();
+            this.divideBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // number1TxtBox
+            // 
+            this.number1TxtBox.Location = new System.Drawing.Point(343, 104);
+            this.number1TxtBox.Name = "number1TxtBox";
+            this.number1TxtBox.Size = new System.Drawing.Size(150, 22);
+            this.number1TxtBox.TabIndex = 0;
+            // 
+            // number2TxtBox
+            // 
+            this.number2TxtBox.Location = new System.Drawing.Point(343, 149);
+            this.number2TxtBox.Name = "number2TxtBox";
+            this.number2TxtBox.Size = new System.Drawing.Size(150, 22);
+            this.number2TxtBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(261, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Number 2";
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(297, 214);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 30);
+            this.AddBtn.TabIndex = 4;
+            this.AddBtn.Text = "Add";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // subtractBtn
+            // 
+            this.subtractBtn.Location = new System.Drawing.Point(418, 214);
+            this.subtractBtn.Name = "subtractBtn";
+            this.subtractBtn.Size = new System.Drawing.Size(75, 30);
+            this.subtractBtn.TabIndex = 5;
+            this.subtractBtn.Text = "Subtract";
+            this.subtractBtn.UseVisualStyleBackColor = true;
+            this.subtractBtn.Click += new System.EventHandler(this.subtractBtn_Click);
+            // 
+            // multiplyBtn
+            // 
+            this.multiplyBtn.Location = new System.Drawing.Point(297, 264);
+            this.multiplyBtn.Name = "multiplyBtn";
+            this.multiplyBtn.Size = new System.Drawing.Size(75, 30);
+            this.multiplyBtn.TabIndex = 6;
+            this.multiplyBtn.Text = "Multiply";
+            this.multiplyBtn.UseVisualStyleBackColor = true;
+            this.multiplyBtn.Click += new System.EventHandler(this.multiplyBtn_Click);
+            // 
+            // divideBtn
+            // 
+            this.divideBtn.Location = new System.Drawing.Point(418, 263);
+            this.divideBtn.Name = "divideBtn";
+            this.divideBtn.Size = new System.Drawing.Size(75, 30);
+            this.divideBtn.TabIndex = 7;
+            this.divideBtn.Text = "Divide";
+            this.divideBtn.UseVisualStyleBackColor = true;
+            this.divideBtn.Click += new System.EventHandler(this.divideBtn_Click);
+            // 
+            // Calulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.divideBtn);
+            this.Controls.Add(this.multiplyBtn);
+            this.Controls.Add(this.subtractBtn);
+            this.Controls.Add(this.AddBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.number2TxtBox);
+            this.Controls.Add(this.number1TxtBox);
+            this.Name = "Calulator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calulator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Calulator_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox number1TxtBox;
+        private System.Windows.Forms.TextBox number2TxtBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button subtractBtn;
+        private System.Windows.Forms.Button multiplyBtn;
+        private System.Windows.Forms.Button divideBtn;
+    }
+}
+
---
+++ Basic C# Practice/SimpleArithmetic/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleArithmetic
+{
+    public partial class Calulator : Form
+    {
+        private bool addButtonClicked = false;
+        private bool subButtonClicked = false;
+        private bool mullButtonClicked = false;
+        private bool divButtonClicked = false;
+
+        public Calulator()
+        {
+            InitializeComponent();
+        }
+
+        private void Calulator_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+        private void Calculation()
+        {
+            try
+            {
+                double num1 = Convert.ToDouble(number1TxtBox.Text);
+                double num2 = Convert.ToDouble(number2TxtBox.Text);
+                double res = 0;
+
+                if (addButtonClicked)
+                {
+                    res = num1 + num2;
+                    addButtonClicked = false;
+                }
+                else if (subButtonClicked)
+                {
+                    res = num1 - num2;
+                    subButtonClicked = false;
+                }
+                else if (mullButtonClicked)
+                {
+                    res = num1 * num2;
+                    mullButtonClicked = false;
+                }
+                else if (divButtonClicked)
+                {
+                    res = num1 / num2;
+                    divButtonClicked = false;
+                }
+                MessageBox.Show(res.ToString());
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Input can't be empty and must be numeric, please enter number first");
+            }
+        }
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            addButtonClicked = true;
+            Calculation();
+        }
+
+        private void subtractBtn_Click(object sender, EventArgs e)
+        {
+            subButtonClicked = true;
+            Calculation();
+        }
+
+        private void multiplyBtn_Click(object sender, EventArgs e)
+        {
+            mullButtonClicked = true;
+            Calculation();
+        }
+
+        private void divideBtn_Click(object sender, EventArgs e)
+        {
+            divButtonClicked = true;
+            Calculation();
+        }
+    }
+}
