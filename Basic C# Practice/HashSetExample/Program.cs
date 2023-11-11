4d437cb9db966dbd1374c8c08526f097d503a6db
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-11 22:23:52 +0600

    HashSet Example added

---
+++ Basic C# Practice/HashSetExample/Program.cs
@@ -0,0 +1,11 @@
+﻿HashSet<string> idNumber = new HashSet<string>();
+
+idNumber.Add("18-37797-2");
+idNumber.Add("18-37797-2"); //will be ignore since same value
+idNumber.Add("18-37798-2");
+idNumber.Add("18-37799-2");
+
+foreach (string id in idNumber)
+{
+    Console.WriteLine(id);
+}
