dd28a75df8e676855a01dd2dee6d1c12ee50c837
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-12 21:35:19 +0600

    value type and reference type example added

---
+++ Basic C# Practice/ValueTypeAndReferenceTypeExample/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //value type
+        int p = 100;
+        int q = p;
+        p = 300;
+
+        Console.WriteLine(p);
+        Console.WriteLine(q);
+
+        //reference type
+        Student s1 = new Student();
+        s1.name = "Rasel";
+        s1.cgpa = 3.59;
+
+        Student s2 = s1;
+
+        Console.WriteLine(s2.name);
+        Console.WriteLine(s2.cgpa);
+
+        s2.name = "Kibria";
+        s2.cgpa = 3.50;
+
+        Console.WriteLine(s1.name);
+        Console.WriteLine(s1.cgpa);
+    }
+}
+
+
+class Student
+{
+    public string name;
+    public double cgpa;
+}
---
+++ Basic C# Practice/ValueTypeAndReferenceTypeExample2/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Student s1 = new Student();
+s1.name = "Rasel";
+s1.cgpa = 3.50;
+
+Console.WriteLine("Before Passing");
+Console.WriteLine(s1.name);
+Console.WriteLine(s1.cgpa);
+
+Console.WriteLine("After method calling");
+ChangeTheStudentInfo(s1);
+Console.WriteLine(s1.name);
+Console.WriteLine(s1.cgpa);
+
+static void ChangeTheStudentInfo(Student student)
+{
+    student.name = "Kibria";
+    student.cgpa = 3.60;
+}
+class Student
+{
+    public string name;
+    public double cgpa;
+}
+
