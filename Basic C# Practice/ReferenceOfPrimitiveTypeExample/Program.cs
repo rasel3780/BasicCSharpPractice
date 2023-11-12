3cf2f4a0fe7ab72fac05d55cb93dc2be382566e4
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-12 21:38:28 +0600

    Reference Of Primitive Type Example

---
+++ Basic C# Practice/ReferenceOfPrimitiveTypeExample/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 10;
+int b = 20;
+
+SwapNum(ref a, ref b);
+Console.WriteLine(a);
+Console.WriteLine(b);
+
+static void SwapNum(ref int x, ref int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
