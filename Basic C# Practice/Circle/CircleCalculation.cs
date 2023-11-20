fbec9a880d79a69326bcbbf86ad2e1e072f86dec
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-20 20:41:49 +0600

    Circle example added

---
+++ Basic C# Practice/Circle/CircleCalculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle
+{
+    public class CircleCalculation
+    {
+        public double radius;
+        
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public double GetDiameter()
+        {
+            return 2 * radius;
+        }
+
+        public double GetPerimeter()
+        {
+            return Math.PI * GetDiameter();
+        }
+    }
+}
---
+++ Basic C# Practice/Circle/Program.cs
@@ -0,0 +1,28 @@
+﻿using Circle;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Enter the radius of circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            CircleCalculation circleCalculation = new CircleCalculation();
+            circleCalculation.radius = radius;
+
+            Console.WriteLine("The area of circle: "+ circleCalculation.GetArea());
+            Console.WriteLine("The Diameter of circle: " + circleCalculation.GetDiameter());
+            Console.WriteLine("The Perimeter of circle: " + circleCalculation.GetPerimeter());
+
+            Console.WriteLine("Do you want to calculate again? press y");
+            string key = Console.ReadLine();
+            
+            if(key.ToUpper() != "Y") 
+            {
+                break;
+            }
+        }
+    }
+}
