050142f21f62dd4cd492b95e7439d034b9bd9844
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-20 23:01:30 +0600

    Result design added

---
+++ Basic C# Practice/Result/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Result
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mathTextBox = new System.Windows.Forms.TextBox();
+            this.physicsTextBox = new System.Windows.Forms.TextBox();
+            this.chemistryTextBox = new System.Windows.Forms.TextBox();
+            this.mathGradeTextBox = new System.Windows.Forms.TextBox();
+            this.physicsGradeTextBox = new System.Windows.Forms.TextBox();
+            this.chemistryGradeTextBox = new System.Windows.Forms.TextBox();
+            this.overallGradeTextBox = new System.Windows.Forms.TextBox();
+            this.averageScoreTextBox = new System.Windows.Forms.TextBox();
+            this.showResultButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // mathTextBox
+            // 
+            this.mathTextBox.Location = new System.Drawing.Point(158, 87);
+            this.mathTextBox.Name = "mathTextBox";
+            this.mathTextBox.Size = new System.Drawing.Size(110, 22);
+            this.mathTextBox.TabIndex = 0;
+            // 
+            // physicsTextBox
+            // 
+            this.physicsTextBox.Location = new System.Drawing.Point(158, 124);
+            this.physicsTextBox.Name = "physicsTextBox";
+            this.physicsTextBox.Size = new System.Drawing.Size(110, 22);
+            this.physicsTextBox.TabIndex = 0;
+            // 
+            // chemistryTextBox
+            // 
+            this.chemistryTextBox.Location = new System.Drawing.Point(158, 161);
+            this.chemistryTextBox.Name = "chemistryTextBox";
+            this.chemistryTextBox.Size = new System.Drawing.Size(110, 22);
+            this.chemistryTextBox.TabIndex = 0;
+            // 
+            // mathGradeTextBox
+            // 
+            this.mathGradeTextBox.Location = new System.Drawing.Point(423, 87);
+            this.mathGradeTextBox.Name = "mathGradeTextBox";
+            this.mathGradeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.mathGradeTextBox.TabIndex = 0;
+            // 
+            // physicsGradeTextBox
+            // 
+            this.physicsGradeTextBox.Location = new System.Drawing.Point(423, 124);
+            this.physicsGradeTextBox.Name = "physicsGradeTextBox";
+            this.physicsGradeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.physicsGradeTextBox.TabIndex = 0;
+            // 
+            // chemistryGradeTextBox
+            // 
+            this.chemistryGradeTextBox.Location = new System.Drawing.Point(423, 161);
+            this.chemistryGradeTextBox.Name = "chemistryGradeTextBox";
+            this.chemistryGradeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.chemistryGradeTextBox.TabIndex = 0;
+            // 
+            // overallGradeTextBox
+            // 
+            this.overallGradeTextBox.Location = new System.Drawing.Point(423, 198);
+            this.overallGradeTextBox.Name = "overallGradeTextBox";
+            this.overallGradeTextBox.Size = new System.Drawing.Size(100, 22);
+            this.overallGradeTextBox.TabIndex = 0;
+            // 
+            // averageScoreTextBox
+            // 
+            this.averageScoreTextBox.Location = new System.Drawing.Point(423, 237);
+            this.averageScoreTextBox.Name = "averageScoreTextBox";
+            this.averageScoreTextBox.Size = new System.Drawing.Size(100, 22);
+            this.averageScoreTextBox.TabIndex = 0;
+            // 
+            // showResultButton
+            // 
+            this.showResultButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.showResultButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.showResultButton.Location = new System.Drawing.Point(158, 215);
+            this.showResultButton.Name = "showResultButton";
+            this.showResultButton.Size = new System.Drawing.Size(110, 23);
+            this.showResultButton.TabIndex = 1;
+            this.showResultButton.Text = "Show Result";
+            this.showResultButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Math";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(98, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Physics";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(86, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Chemistry";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(340, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 16);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Math Grade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 16);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Physics Grade";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(310, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 16);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Chemistry Grade";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 16);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Overall Grade";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(319, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(98, 16);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Average Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showResultButton);
+            this.Controls.Add(this.averageScoreTextBox);
+            this.Controls.Add(this.overallGradeTextBox);
+            this.Controls.Add(this.chemistryGradeTextBox);
+            this.Controls.Add(this.physicsGradeTextBox);
+            this.Controls.Add(this.mathGradeTextBox);
+            this.Controls.Add(this.chemistryTextBox);
+            this.Controls.Add(this.physicsTextBox);
+            this.Controls.Add(this.mathTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox mathTextBox;
+        private System.Windows.Forms.TextBox physicsTextBox;
+        private System.Windows.Forms.TextBox chemistryTextBox;
+        private System.Windows.Forms.TextBox mathGradeTextBox;
+        private System.Windows.Forms.TextBox physicsGradeTextBox;
+        private System.Windows.Forms.TextBox chemistryGradeTextBox;
+        private System.Windows.Forms.TextBox overallGradeTextBox;
+        private System.Windows.Forms.TextBox averageScoreTextBox;
+        private System.Windows.Forms.Button showResultButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
