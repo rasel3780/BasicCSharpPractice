08444a03c76183f4cf91a9ba01fbe4cb11f4801c
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-20 21:53:33 +0600

    Salary Calculator design added

---
+++ Basic C# Practice/SalaryCalculator/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace SalaryCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.empNameTextBox = new System.Windows.Forms.TextBox();
+            this.basicAmountTextBox = new System.Windows.Forms.TextBox();
+            this.houseRentTextBox = new System.Windows.Forms.TextBox();
+            this.medicalAllowanceTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // empNameTextBox
+            // 
+            this.empNameTextBox.Location = new System.Drawing.Point(252, 109);
+            this.empNameTextBox.Name = "empNameTextBox";
+            this.empNameTextBox.Size = new System.Drawing.Size(259, 22);
+            this.empNameTextBox.TabIndex = 0;
+            // 
+            // basicAmountTextBox
+            // 
+            this.basicAmountTextBox.Location = new System.Drawing.Point(252, 155);
+            this.basicAmountTextBox.Name = "basicAmountTextBox";
+            this.basicAmountTextBox.Size = new System.Drawing.Size(170, 22);
+            this.basicAmountTextBox.TabIndex = 0;
+            // 
+            // houseRentTextBox
+            // 
+            this.houseRentTextBox.Location = new System.Drawing.Point(252, 202);
+            this.houseRentTextBox.Name = "houseRentTextBox";
+            this.houseRentTextBox.Size = new System.Drawing.Size(170, 22);
+            this.houseRentTextBox.TabIndex = 0;
+            // 
+            // medicalAllowanceTextBox
+            // 
+            this.medicalAllowanceTextBox.Location = new System.Drawing.Point(252, 248);
+            this.medicalAllowanceTextBox.Name = "medicalAllowanceTextBox";
+            this.medicalAllowanceTextBox.Size = new System.Drawing.Size(170, 22);
+            this.medicalAllowanceTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(137, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(157, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Basic Amount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "House Rent";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(126, 254);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Medical Allowance";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(281, 292);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(111, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show Total";
+            this.showButton.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(428, 205);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "% of basic";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(428, 251);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "% of basic";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.medicalAllowanceTextBox);
+            this.Controls.Add(this.houseRentTextBox);
+            this.Controls.Add(this.basicAmountTextBox);
+            this.Controls.Add(this.empNameTextBox);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox empNameTextBox;
+        private System.Windows.Forms.TextBox basicAmountTextBox;
+        private System.Windows.Forms.TextBox houseRentTextBox;
+        private System.Windows.Forms.TextBox medicalAllowanceTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
