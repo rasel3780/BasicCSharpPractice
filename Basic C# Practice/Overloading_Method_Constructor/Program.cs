907f6ab2a112a6fe26adfcbc2dd6ff36216a45f8
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-23 11:05:39 +0600

    Method and constructor overloading added

---
+++ Basic C# Practice/Overloading_Method_Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Overloading_Method_Constructor;
+
+
+
+Student student1 = new Student("st-01", "Rasel");
+Student student2 = new Student("st-02", "Kibira", "kibria.example@gmail.com");
+Student student3 = new Student();
+
+student1.ShowInfo(student1.Id);
+student1.ShowInfo(true);
+
+student2.ShowInfo(student2.Id);
+student2.ShowInfo(true);
+
+student3.ShowInfo(true);
+
---
+++ Basic C# Practice/Overloading_Method_Constructor/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Method_Constructor
+{
+    internal class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        public Student()
+        {
+            Id = "Unknown";
+            Name = "Unknown";
+            Email = "Unknown";
+        }
+
+        public Student(string id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Student(string id, string name, string email) : this(id, name) 
+        {
+            Email = email;
+        }
+
+        public void ShowInfo(string id)
+        {
+            Console.WriteLine($"Name:{Name}");
+            Console.WriteLine("-----------------");
+        }
+
+        public void ShowInfo(bool showDetails)
+        {
+            if (showDetails)
+            {
+                Console.WriteLine($"Id:{Id}, Name:{Name}, Email:{Email}");
+                Console.WriteLine("-----------------");
+            }
+        }
+    }
+}
