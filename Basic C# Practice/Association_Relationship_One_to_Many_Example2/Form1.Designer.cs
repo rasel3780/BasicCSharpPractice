9e54d0821ed066156d16ecc571396cbca3a8b1c2
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-25 00:17:08 +0600

    Association_Relationship_One_to_Many_Example2 Form design added

---
+++ Basic C# Practice/Association_Relationship_One_to_Many_Example2/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Association_Relationship_One_to_Many_Example2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.showShopNameTextBox = new System.Windows.Forms.TextBox();
+            this.numberOfProductTextBox = new System.Windows.Forms.TextBox();
+            this.productListBox = new System.Windows.Forms.ListBox();
+            this.shopSaveButton = new System.Windows.Forms.Button();
+            this.productSaveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 251);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(136, 295);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // showShopNameTextBox
+            // 
+            this.showShopNameTextBox.Location = new System.Drawing.Point(559, 92);
+            this.showShopNameTextBox.Name = "showShopNameTextBox";
+            this.showShopNameTextBox.Size = new System.Drawing.Size(246, 22);
+            this.showShopNameTextBox.TabIndex = 0;
+            // 
+            // numberOfProductTextBox
+            // 
+            this.numberOfProductTextBox.Location = new System.Drawing.Point(559, 139);
+            this.numberOfProductTextBox.Name = "numberOfProductTextBox";
+            this.numberOfProductTextBox.Size = new System.Drawing.Size(246, 22);
+            this.numberOfProductTextBox.TabIndex = 0;
+            // 
+            // productListBox
+            // 
+            this.productListBox.FormattingEnabled = true;
+            this.productListBox.ItemHeight = 16;
+            this.productListBox.Location = new System.Drawing.Point(559, 196);
+            this.productListBox.Name = "productListBox";
+            this.productListBox.Size = new System.Drawing.Size(246, 180);
+            this.productListBox.TabIndex = 1;
+            // 
+            // shopSaveButton
+            // 
+            this.shopSaveButton.Location = new System.Drawing.Point(202, 168);
+            this.shopSaveButton.Name = "shopSaveButton";
+            this.shopSaveButton.Size = new System.Drawing.Size(75, 23);
+            this.shopSaveButton.TabIndex = 2;
+            this.shopSaveButton.Text = "Save";
+            this.shopSaveButton.UseVisualStyleBackColor = true;
+            // 
+            // productSaveButton
+            // 
+            this.productSaveButton.Location = new System.Drawing.Point(202, 323);
+            this.productSaveButton.Name = "productSaveButton";
+            this.productSaveButton.Size = new System.Drawing.Size(75, 23);
+            this.productSaveButton.TabIndex = 2;
+            this.productSaveButton.Text = "Save";
+            this.productSaveButton.UseVisualStyleBackColor = true;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(643, 382);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(86, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Address";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(180, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Shop Information";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(180, 232);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product Information";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(86, 254);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Code";
+            this.label5.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 298);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Quantity";
+            this.label6.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(494, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 16);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Name";
+            this.label7.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(436, 142);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(117, 16);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Number of product";
+            this.label8.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.productSaveButton);
+            this.Controls.Add(this.shopSaveButton);
+            this.Controls.Add(this.productListBox);
+            this.Controls.Add(this.numberOfProductTextBox);
+            this.Controls.Add(this.showShopNameTextBox);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox showShopNameTextBox;
+        private System.Windows.Forms.TextBox numberOfProductTextBox;
+        private System.Windows.Forms.ListBox productListBox;
+        private System.Windows.Forms.Button shopSaveButton;
+        private System.Windows.Forms.Button productSaveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
