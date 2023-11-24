cbdb29fcab2b4db64f3460aaca4a4590298b1e63
Rasel Kibria <rasel5jkibria@gmail.com>
2023-11-24 19:02:29 +0600

    Association_Relationship_One_To_One_Example2 form design added

---
+++ Basic C# Practice/Association_Relationship_One_To_One_Example2/Form1.Designer.cs
@@ -0,0 +1,376 @@
+﻿namespace Association_Relationship_One_To_One_Example2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.basicTextBox = new System.Windows.Forms.TextBox();
+            this.medicalTextBox = new System.Windows.Forms.TextBox();
+            this.conveyanceTextBox = new System.Windows.Forms.TextBox();
+            this.showIdTextBox = new System.Windows.Forms.TextBox();
+            this.showNameTextBox = new System.Windows.Forms.TextBox();
+            this.showEmailTextBox = new System.Windows.Forms.TextBox();
+            this.showBasicTextBox = new System.Windows.Forms.TextBox();
+            this.showMedicalTextBox = new System.Windows.Forms.TextBox();
+            this.showConveyanceTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(141, 79);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.Size = new System.Drawing.Size(207, 22);
+            this.idTextBox.TabIndex = 0;
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(141, 124);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(207, 22);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(141, 172);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(207, 22);
+            this.emailTextBox.TabIndex = 0;
+            // 
+            // basicTextBox
+            // 
+            this.basicTextBox.Location = new System.Drawing.Point(141, 265);
+            this.basicTextBox.Name = "basicTextBox";
+            this.basicTextBox.Size = new System.Drawing.Size(207, 22);
+            this.basicTextBox.TabIndex = 0;
+            // 
+            // medicalTextBox
+            // 
+            this.medicalTextBox.Location = new System.Drawing.Point(141, 307);
+            this.medicalTextBox.Name = "medicalTextBox";
+            this.medicalTextBox.Size = new System.Drawing.Size(207, 22);
+            this.medicalTextBox.TabIndex = 0;
+            // 
+            // conveyanceTextBox
+            // 
+            this.conveyanceTextBox.Location = new System.Drawing.Point(141, 359);
+            this.conveyanceTextBox.Name = "conveyanceTextBox";
+            this.conveyanceTextBox.Size = new System.Drawing.Size(207, 22);
+            this.conveyanceTextBox.TabIndex = 0;
+            // 
+            // showIdTextBox
+            // 
+            this.showIdTextBox.Location = new System.Drawing.Point(531, 79);
+            this.showIdTextBox.Name = "showIdTextBox";
+            this.showIdTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showIdTextBox.TabIndex = 0;
+            // 
+            // showNameTextBox
+            // 
+            this.showNameTextBox.Location = new System.Drawing.Point(531, 124);
+            this.showNameTextBox.Name = "showNameTextBox";
+            this.showNameTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showNameTextBox.TabIndex = 0;
+            // 
+            // showEmailTextBox
+            // 
+            this.showEmailTextBox.Location = new System.Drawing.Point(531, 172);
+            this.showEmailTextBox.Name = "showEmailTextBox";
+            this.showEmailTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showEmailTextBox.TabIndex = 0;
+            // 
+            // showBasicTextBox
+            // 
+            this.showBasicTextBox.Location = new System.Drawing.Point(531, 214);
+            this.showBasicTextBox.Name = "showBasicTextBox";
+            this.showBasicTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showBasicTextBox.TabIndex = 0;
+            // 
+            // showMedicalTextBox
+            // 
+            this.showMedicalTextBox.Location = new System.Drawing.Point(531, 256);
+            this.showMedicalTextBox.Name = "showMedicalTextBox";
+            this.showMedicalTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showMedicalTextBox.TabIndex = 0;
+            // 
+            // showConveyanceTextBox
+            // 
+            this.showConveyanceTextBox.Location = new System.Drawing.Point(531, 308);
+            this.showConveyanceTextBox.Name = "showConveyanceTextBox";
+            this.showConveyanceTextBox.Size = new System.Drawing.Size(207, 22);
+            this.showConveyanceTextBox.TabIndex = 0;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(203, 403);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 1;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(595, 358);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 1;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(83, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(88, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(83, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Basic";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(61, 309);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Medical";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(38, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Conveyance";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(502, 81);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Id";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(473, 126);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Name";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(480, 216);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 20);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Basic";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(478, 174);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 20);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "Email";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(458, 256);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(67, 20);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Medical";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(428, 307);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(97, 20);
+            this.label12.TabIndex = 2;
+            this.label12.Text = "Conveyance";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(170, 230);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(141, 20);
+            this.label13.TabIndex = 2;
+            this.label13.Text = "Salary Information";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(170, 47);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(170, 20);
+            this.label14.TabIndex = 2;
+            this.label14.Text = "Employee Information";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.showConveyanceTextBox);
+            this.Controls.Add(this.conveyanceTextBox);
+            this.Controls.Add(this.showMedicalTextBox);
+            this.Controls.Add(this.medicalTextBox);
+            this.Controls.Add(this.showBasicTextBox);
+            this.Controls.Add(this.basicTextBox);
+            this.Controls.Add(this.showEmailTextBox);
+            this.Controls.Add(this.showNameTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.showIdTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.TextBox basicTextBox;
+        private System.Windows.Forms.TextBox medicalTextBox;
+        private System.Windows.Forms.TextBox conveyanceTextBox;
+        private System.Windows.Forms.TextBox showIdTextBox;
+        private System.Windows.Forms.TextBox showNameTextBox;
+        private System.Windows.Forms.TextBox showEmailTextBox;
+        private System.Windows.Forms.TextBox showBasicTextBox;
+        private System.Windows.Forms.TextBox showMedicalTextBox;
+        private System.Windows.Forms.TextBox showConveyanceTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+    }
+}
+
