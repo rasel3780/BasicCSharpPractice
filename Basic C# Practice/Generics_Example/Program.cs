b94314c91965cf24ad43987c92c8b393a232a3e7
Rasel Kibria <rasel5jkibria@gmail.com>
2023-12-03 23:34:05 +0600

    Generics_Example added

---
+++ Basic C# Practice/Generics_Example/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+static void Swap<T>(ref T a, ref T b)
+{
+    T temp = a;
+    a = b; 
+    b = temp;
+}
+
+int a = 5;
+int b = 10;
+Console.WriteLine(a + "," + b);
+
+Swap(ref a, ref b);
+Console.WriteLine(a + "," + b);
+
+string firstName = "Rasel";
+string lastName = "Kibria";
+Console.WriteLine(firstName + " " + lastName);
+
+Swap(ref firstName, ref lastName);
+Console.WriteLine(firstName + " " + lastName);
