c5e5515b08c19410305585ea71a0474dac9556e2
Rasel Kibria <rasel5jkibria@gmail.com>
2023-12-04 15:24:56 +0600

    IComparable_Example added

---
+++ Basic C# Practice/IComparable_Example/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IComparable_Example
+{
+    internal class Book: IComparable<Book>
+    {
+        public string Title { get; set; }
+        public double Price { get; set; }
+
+        public int CompareTo(Book bookToCompare)
+        {
+            if(this.Price > bookToCompare.Price)
+                return 1;
+
+            else if(this.Price < bookToCompare.Price)
+                return -1;
+
+            else
+                return 0;
+        }
+    }
+}
---
+++ Basic C# Practice/IComparable_Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using IComparable_Example;
+
+List<Book> books = new List<Book>();
+
+books.Add(new Book(){ Title = "Java", Price = 1150 });
+books.Add(new Book(){ Title = "ASP .Net", Price = 1200 });
+
+books.Sort();
+
+foreach (Book book in books)
+{
+    Console.WriteLine(book.Title + "," + book.Price);
+}
