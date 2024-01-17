585aed896365f77df6a46426ac74ae5adae55992
Rasel Kibria <rasel5jkibria@gmail.com>
2024-01-17 20:24:45 +0600

    Reflection1 example added

---
+++ Basic C# Practice/Reflection1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection1
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        private int Age { get; set; }
+
+        public Person(int age)
+        {
+            Age = age;
+        }
+    }
+}
---
+++ Basic C# Practice/Reflection1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Reflection1;
+using System.Reflection;
+
+string className = Console.ReadLine();
+string propertyName = Console.ReadLine();
+string propertyValue = Console.ReadLine();
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+Type type = assembly.GetType(className);
+
+PropertyInfo property = type.GetProperty(propertyName);
+ConstructorInfo constructor = type.GetConstructor(new Type[] {typeof(int)});
+
+object instance = constructor.Invoke(new object[] {20 });
+property.SetValue(instance, propertyValue);
+
+foreach(var p in type.GetProperties(BindingFlags.Instance| BindingFlags.Public | BindingFlags.NonPublic))
+{
+    Console.WriteLine($"{p.Name}: {p.GetValue(instance)}");
+}
