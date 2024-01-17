9f6d281efda95b94975a488a35e78078b51cb234
Rasel Kibria <rasel5jkibria@gmail.com>
2024-01-17 20:09:49 +0600

    Reflection example added

---
+++ Basic C# Practice/Reflection/Program.cs
@@ -0,0 +1,18 @@
+﻿using Reflection;
+using System.Reflection;
+
+string className = Console.ReadLine();
+string propertyName = Console.ReadLine();
+string propertyValue = Console.ReadLine();
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+Type type = assembly.GetType(className);
+
+PropertyInfo property = type.GetProperty(propertyName);
+ConstructorInfo constructor = type.GetConstructor(new Type[] { });
+
+object instance = constructor.Invoke(new object[] { });
+property.SetValue(instance, propertyValue);
+
+Person person = instance as Person;
+Console.WriteLine(person.Name);
